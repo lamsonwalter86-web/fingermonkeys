536257e5e743c1a5d534078ddf6bf4459cc9d8a7
Taiga <you@example.com>
2025-09-23 04:57:56 -0700

    first commit

---
+++ site.config.ts
@@ -0,0 +1,22 @@
+export const siteConfig = {
+  title: "Finger Monkeys Hunt",
+  description: "Finger Monkeys Hunt",
+  organisation: "Finger Monkeys",
+  tokenName: "FMT",
+  tokenShortName: "FMT",
+  // Если лендинг позиционируется как отдельный сайт для airdrop'а
+  mainSite: "",
+  twitterUrl: "https://twitter.com/fingermonkeysio",
+  telegramUrl: "",
+  redditUrl: "/",
+  coinmarketcupUrl: "https://coinmarketcap.com/currencies/fingermonkeys/",
+  theme: "dark",
+  color: "#9FD919",
+  faviconIcoUrl: "https://i-p.rmcdn.net/64f4527e45849b003dc9348a/5167502/image-6fb68a45-3e59-4970-bd09-904e205d0171.png?w=115&e=webp&nll=true&cX=109&cY=0&cW=2709&cH=2817",
+  // logoUrl: "https://app.ethena.fi/custom/logo.svg",
+  // mainImageUrl: "https://app.ethena.fi/shared/ethena.svg",
+  mainImageUrl:
+    "./token-3d.png",
+  mintSumUsd: 1100000,
+  timeSec: 60 * 60,
+};
